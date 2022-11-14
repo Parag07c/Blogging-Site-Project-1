7fbdbd0a2fb3cfbfe367855c9e28d16c03ef09e3
wasim86 <mohdwasim1996@gmail.com>
2022-11-14 16:24:44 +0530

    done

--- src/route/route.js
+++ src/route/route.js
@@ -1,1 +1,16 @@
 
+const express = require('express');
+const router = express.Router();
+const authorModel= require("../models/authoModel.js")
+const blogModel = require("../models/blogModel.js")
+const authorController= require("../controllers/authorController")
+const blogController= require("../controllers/blogController")
+
+
+router.get("/test-me", function (req, res) {
+    res.send("My first project!")
+})
+  
+
+
+module.exports = router;
